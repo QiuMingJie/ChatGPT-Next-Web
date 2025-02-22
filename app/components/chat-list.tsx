65f284159b5a0d5dbf683874f666f9d165099ea1
cain <826292460@qq.com>
2025-02-22 22:11:19 +0800

    session会影响打包

--- app/components/chat-list.tsx
+++ app/components/chat-list.tsx
@@ -10,7 +10,6 @@ import { useChatStore } from "../store";
 
 import Locale from "../locales";
 import { useLocation, useNavigate } from "react-router-dom";
-import { nanoid } from "nanoid";
 import { Path } from "../constant";
 import { MaskAvatar } from "./mask";
 import { Mask } from "../store/mask";
@@ -129,142 +128,142 @@ export function ChatList(props: { narrow?: boolean }) {
 
     moveSession(source.index, destination.index);
   };
-  const list = [
-    {
-      id: nanoid(),
-      topic: "诊断练习",
-      memoryPrompt: "",
-      messages: [],
-      stat: { tokenCount: 0, wordCount: 0, charCount: 0 },
-      lastUpdate: Date.now(),
-      lastSummarizeIndex: 0,
-      mask: {
-        avatar: "270d-fe0f",
-        name: "诊断练习",
-        context: [
-          {
-            id: "writer-0",
-            role: "user",
-            content: "我给你关键字，你发给我一些训练题目，我的关键字是：",
-            date: "",
-          },
-        ],
-        modelConfig: {
-          model: "deepseek",
-          providerName: "Alibaba",
-          temperature: 1,
-          top_p: 1,
-          max_tokens: 2000,
-          presence_penalty: 0,
-          frequency_penalty: 0,
-          sendMemory: true,
-          historyMessageCount: 4,
-          compressMessageLengthThreshold: 1000,
-          compressModel: "",
-          compressProviderName: "",
-          enableInjectSystemPrompts: true,
-          template: "{{input}}",
-          size: "1024x1024",
-          quality: "standard",
-          style: "vivid",
-        },
-        lang: "cn",
-        builtin: true,
-        createdAt: Date.now(),
-        id: 100001,
-      },
-    },
-    {
-      id: nanoid(),
-      topic: "智能问答",
-      memoryPrompt: "",
-      messages: [],
-      stat: { tokenCount: 0, wordCount: 0, charCount: 0 },
-      lastUpdate: Date.now(),
-      lastSummarizeIndex: 0,
-      mask: {
-        avatar: "1f978",
-        name: "智能问答",
-        context: [
-          {
-            id: "writer-0",
-            role: "user",
-            content:
-              "我现在开始会问你一些问题，你根据你所知的知识回答我，我的问题是：",
-            date: "",
-          },
-        ],
-        modelConfig: {
-          model: "deepseek",
-          providerName: "Alibaba",
-          temperature: 1,
-          top_p: 1,
-          max_tokens: 2000,
-          presence_penalty: 0,
-          frequency_penalty: 0,
-          sendMemory: true,
-          historyMessageCount: 4,
-          compressMessageLengthThreshold: 1000,
-          compressModel: "",
-          compressProviderName: "",
-          enableInjectSystemPrompts: true,
-          template: "{{input}}",
-          size: "1024x1024",
-          quality: "standard",
-          style: "vivid",
-        },
-        lang: "cn",
-        builtin: true,
-        createdAt: Date.now(),
-        id: 100000,
-      },
-    },
-    {
-      id: nanoid(),
-      topic: "知识库",
-      memoryPrompt: "",
-      messages: [],
-      stat: { tokenCount: 0, wordCount: 0, charCount: 0 },
-      lastUpdate: Date.now(),
-      lastSummarizeIndex: 0,
-      mask: {
-        avatar: "1f4da",
-        name: "知识库",
-        context: [
-          {
-            id: "writer-0",
-            role: "user",
-            content:
-              "你整合一下中医诊断学资料，然后我会发给你关键词，你根据我的关键词发给我对应的一些中医诊断资料，我的第一个关键词是：",
-            date: "",
-          },
-        ],
-        modelConfig: {
-          model: "deepseek",
-          providerName: "Alibaba",
-          temperature: 1,
-          top_p: 1,
-          max_tokens: 2000,
-          presence_penalty: 0,
-          frequency_penalty: 0,
-          sendMemory: true,
-          historyMessageCount: 4,
-          compressMessageLengthThreshold: 1000,
-          compressModel: "",
-          compressProviderName: "",
-          enableInjectSystemPrompts: true,
-          template: "{{input}}",
-          size: "1024x1024",
-          quality: "standard",
-          style: "vivid",
-        },
-        lang: "cn",
-        builtin: true,
-        createdAt: Date.now(),
-        id: 100002,
-      },
-    },
-  ];
+  // const list = [
+  //   {
+  //     id: nanoid(),
+  //     topic: "诊断练习",
+  //     memoryPrompt: "",
+  //     messages: [],
+  //     stat: { tokenCount: 0, wordCount: 0, charCount: 0 },
+  //     lastUpdate: Date.now(),
+  //     lastSummarizeIndex: 0,
+  //     mask: {
+  //       avatar: "270d-fe0f",
+  //       name: "诊断练习",
+  //       context: [
+  //         {
+  //           id: "writer-0",
+  //           role: "user",
+  //           content: "我给你关键字，你发给我一些训练题目，我的关键字是：",
+  //           date: "",
+  //         },
+  //       ],
+  //       modelConfig: {
+  //         model: "deepseek",
+  //         providerName: "Alibaba",
+  //         temperature: 1,
+  //         top_p: 1,
+  //         max_tokens: 2000,
+  //         presence_penalty: 0,
+  //         frequency_penalty: 0,
+  //         sendMemory: true,
+  //         historyMessageCount: 4,
+  //         compressMessageLengthThreshold: 1000,
+  //         compressModel: "",
+  //         compressProviderName: "",
+  //         enableInjectSystemPrompts: true,
+  //         template: "{{input}}",
+  //         size: "1024x1024",
+  //         quality: "standard",
+  //         style: "vivid",
+  //       },
+  //       lang: "cn",
+  //       builtin: true,
+  //       createdAt: Date.now(),
+  //       id: 100001,
+  //     },
+  //   },
+  //   {
+  //     id: nanoid(),
+  //     topic: "智能问答",
+  //     memoryPrompt: "",
+  //     messages: [],
+  //     stat: { tokenCount: 0, wordCount: 0, charCount: 0 },
+  //     lastUpdate: Date.now(),
+  //     lastSummarizeIndex: 0,
+  //     mask: {
+  //       avatar: "1f978",
+  //       name: "智能问答",
+  //       context: [
+  //         {
+  //           id: "writer-0",
+  //           role: "user",
+  //           content:
+  //             "我现在开始会问你一些问题，你根据你所知的知识回答我，我的问题是：",
+  //           date: "",
+  //         },
+  //       ],
+  //       modelConfig: {
+  //         model: "deepseek",
+  //         providerName: "Alibaba",
+  //         temperature: 1,
+  //         top_p: 1,
+  //         max_tokens: 2000,
+  //         presence_penalty: 0,
+  //         frequency_penalty: 0,
+  //         sendMemory: true,
+  //         historyMessageCount: 4,
+  //         compressMessageLengthThreshold: 1000,
+  //         compressModel: "",
+  //         compressProviderName: "",
+  //         enableInjectSystemPrompts: true,
+  //         template: "{{input}}",
+  //         size: "1024x1024",
+  //         quality: "standard",
+  //         style: "vivid",
+  //       },
+  //       lang: "cn",
+  //       builtin: true,
+  //       createdAt: Date.now(),
+  //       id: 100000,
+  //     },
+  //   },
+  //   {
+  //     id: nanoid(),
+  //     topic: "知识库",
+  //     memoryPrompt: "",
+  //     messages: [],
+  //     stat: { tokenCount: 0, wordCount: 0, charCount: 0 },
+  //     lastUpdate: Date.now(),
+  //     lastSummarizeIndex: 0,
+  //     mask: {
+  //       avatar: "1f4da",
+  //       name: "知识库",
+  //       context: [
+  //         {
+  //           id: "writer-0",
+  //           role: "user",
+  //           content:
+  //             "你整合一下中医诊断学资料，然后我会发给你关键词，你根据我的关键词发给我对应的一些中医诊断资料，我的第一个关键词是：",
+  //           date: "",
+  //         },
+  //       ],
+  //       modelConfig: {
+  //         model: "deepseek",
+  //         providerName: "Alibaba",
+  //         temperature: 1,
+  //         top_p: 1,
+  //         max_tokens: 2000,
+  //         presence_penalty: 0,
+  //         frequency_penalty: 0,
+  //         sendMemory: true,
+  //         historyMessageCount: 4,
+  //         compressMessageLengthThreshold: 1000,
+  //         compressModel: "",
+  //         compressProviderName: "",
+  //         enableInjectSystemPrompts: true,
+  //         template: "{{input}}",
+  //         size: "1024x1024",
+  //         quality: "standard",
+  //         style: "vivid",
+  //       },
+  //       lang: "cn",
+  //       builtin: true,
+  //       createdAt: Date.now(),
+  //       id: 100002,
+  //     },
+  //   },
+  // ];
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="chat-list">
@@ -275,7 +274,7 @@ export function ChatList(props: { narrow?: boolean }) {
             {...provided.droppableProps}
           >
             {/* {JSON.stringify(sessions)} */}
-            {list.map((item, i) => (
+            {sessions.map((item, i) => (
               <ChatItem
                 title={item.topic}
                 time={new Date(item.lastUpdate).toLocaleString()}
